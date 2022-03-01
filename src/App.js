a4e954b2953564533b8d790d7543a56609fa96e1
vivek <viveky276@gmail.com>
2022-03-01 22:56:51 +0530

    List of repos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import github from "./db";
+import { useEffect, useState, useCallback } from "react";
+import query from "./Query";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [userName, setUserName] = useState("");
+    let [repoList, setRepoList] = useState(null);
+    let fetchData = useCallback(() => {
+        fetch(github.baseURL, {
+            method: "POST",
+            headers: github.headers,
+            body: JSON.stringify(query)
+        })
+        .then(response => response.json())
+        .then(data => {
+            const viewer = data.data.viewer; 
+            setUserName(viewer.name);
+            setRepoList(viewer.repositories.nodes);
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return (
+        <div className="App container mt-5">
+            <h1 className="text-primary">
+                <i className="bi bi-diagram-2-fill"></i> Repos
+            </h1>
+            <p>
+                Hey there {userName}
+            </p>
+            { repoList && (
+                <ul className="list-group list-group-flush">
+                    { repoList.map((repo) => (
+                            <li className="list-group-item" key={repo.id.toString()}>
+                                <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                                    {repo.name}
+                                </a>
+                                <p className="small">{repo.description}</p>
+                            </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Query.js
@@ -0,0 +1,19 @@
+const githubQuery = {
+    query: `
+        {
+            viewer {
+                name
+                repositories(first: 10) {
+                    nodes {
+                        name
+                        description
+                        id
+                        url
+                    }
+                }
+            }
+        } 
+    `
+};
+
+export default githubQuery;
