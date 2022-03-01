72f83be1ce2822a34909f5fe84d04014ff32d27d
vivek <viveky276@gmail.com>
2022-03-01 21:55:06 +0530

    adding db.js file without token

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseURL: "https://api.github.com/graphql",
+    username: "vive1994k",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer `create-and-put-personal-access token`"
+    }
+}
+
+export default github;
